8a4c055b3ae41fb93343ce621300956630d72ce6
Noah Lundquist <nalundquist@gmail.com>
2022-12-02 16:49:41 -0800

    add startpage and further css formatting

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,5 +1,22 @@
 function StartPage(){
+	const startStyle = {
+		postion: 'absolute',
+		top: '35%',
+		left: '50%',
+		textAlign: 'center'
+	}
+	return(
 	<React.Fragment>
-		<div class=''></div>
+		<div class={startStyle}>
+			<div class='titlefade'>
+				<p>"You are new here,</p><p> I think.<hr/></p>
+				<p>Many years ago</p><p>I used to live in this house...</p>	
+			</div>
+			<div class='newgamebox'>
+				<p>An Amusement?</p>
+				<button>Yes</button>
+			</div>
+		</div>
 	</React.Fragment>
+	)
 }
