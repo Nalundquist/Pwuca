f50220874ed23a0b5af220a0815c1d5e19ab2bd7
Noah Lundquist <nalundquist@gmail.com>
2022-12-06 13:37:16 -0800

    add first state changes to BodyControl

--- src/components/BodyControl.js
+++ src/components/BodyControl.js
@@ -1,13 +1,28 @@
+import React, {setState, useEffect} from 'react';
 import StartPage from './StartPage';
+import NewGame from './NewGame';
 
 function BodyControl(){
 	const bodyStyle = {
 		margin: '15px'
 	}
 
+	const [newGameVisible, setNewGameVisible] = setState(false);
+
+	const handleNewGameForm = () => {
+		setNewGameVisible(!newGameVisible);
+	}
+
+	let visiblePageElement;
+
+	if (newGameVisible){
+		visiblePageElement = <NewGame />
+	} else {
+		visiblePageElement = <StartPage newGameOnClick={handleNewGameForm} />
+	}
 	return(
 		<div style={bodyStyle}>
-			<StartPage />
+			{visiblePageElement}
 		</div>
 	)
 }
