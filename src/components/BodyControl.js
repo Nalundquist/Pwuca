ba250e955f2e381f8f0a0a921ad42b0a00a8a0e3
Noah Lundquist <nalundquist@gmail.com>
2022-12-18 22:11:51 -0800

    add selectRoom, alter handleMakeRoom

--- src/components/BodyControl.js
+++ src/components/BodyControl.js
@@ -17,25 +17,25 @@ function BodyControl(){
 	const [newGameVisible, setNewGameVisible] = useState(false);
 	const [playerNamePromptVisible, setPlayerNamePromptVisible] = useState(false);
 	const [gameBoardVisible, setGameBoardVisible] = useState(false)
-	const [playerQuantity, setPlayerQuantity] = useState(null);
-	const [playerNames, setPlayerNames] = useState([])
-	const [players, setPlayers] = useState([])
+	// const [playerQuantity, setPlayerQuantity] = useState(null);
+	// const [playerNames, setPlayerNames] = useState([])
+	// const [players, setPlayers] = useState([])
 
 	const handleNewGameForm = () => {
 		setNewGameVisible(!newGameVisible);
 	}
 
 	const handleMakeRoom = async () => {
-		const thisPlayer = {
+		const currentPlayer = {
 			name: auth.currentUser.displayName,
 			pwuca: "",
 			turnOrder: null,
 			isTurn: false,
 			id: auth.currentUser.uid
 		}
-		await setPlayers(thisPlayer);
+
 		const room = {
-			playerList: players,
+			playerList: [currentPlayer],
 			id: v4(),
 			shareId: nanoid(6),
 			word: "",
@@ -46,17 +46,31 @@ function BodyControl(){
 		}
 		await addDoc(collection(db, "rooms"), room);
 	}
-	
+
+	const selectRoom = async (shareId, userId) => {
+		const selectedRoom = roomList.filter(room => room.id === id)[0];
+		const roomPlayerList = selectedRoom.playerList;
+		const currentPlayer = {
+			name: auth.currentUser.displayName,
+			pwuca: "",
+			turnOrder: null,
+			isTurn: false,
+			id: auth.currentUser.uid
+		}
+		roomPlayerList.concat(currentPlayer);
+		await setRoom(selectedRoom);
+	}
+
 	const handlePlayerNumber = (players) => {
 		setPlayerQuantity(players);
 		setPlayerNamePromptVisible(true);
 	}
 
-	const handlePlayerName = (index, event) => {
-		let newNameList = [...playerNames];
-		newNameList[index] = event.target.value;
-		setPlayerNames(newNameList);
-	}
+	// const handlePlayerName = (index, event) => {
+	// 	let newNameList = [...playerNames];
+	// 	newNameList[index] = event.target.value;
+	// 	setPlayerNames(newNameList);
+	// }
 
 	const randomTurnOrder = (playerList) => {
 		let lengthCount = playerList.length, randIndex;
@@ -69,21 +83,21 @@ function BodyControl(){
 		}
 		return playerList;
 	}
-	const handleNewPlayers = (playersNames) => {
-		let playersList = []
-		playersNames.forEach(playerName => {
-			playersList = playersList.concat({
-				name: auth.currentUser.displayName,
-				pwuca: "",
-				turnOrder: null,
-				isTurn: false,
-				id: auth.currentUser.uid
-			})
-		});
-		setPlayers(randomTurnOrder(playersList));
-		setNewGameVisible(false);
-		setGameBoardVisible(true);
-	}
+	// const handleNewPlayers = (playersNames) => {
+	// 	let playersList = []
+	// 	playersNames.forEach(playerName => {
+	// 		playersList = playersList.concat({
+	// 			name: auth.currentUser.displayName,
+	// 			pwuca: "",
+	// 			turnOrder: null,
+	// 			isTurn: false,
+	// 			id: auth.currentUser.uid
+	// 		})
+	// 	});
+	// 	setPlayers(randomTurnOrder(playersList));
+	// 	setNewGameVisible(false);
+	// 	setGameBoardVisible(true);
+	// }
 
 	const changePlayerScore = (thisPlayer) => {
 		if(thisPlayer.pwuca === ""){
